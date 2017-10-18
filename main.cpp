c9d057b2b60e63e53dcb724649651115b2bdb7cd
jhoke97 <jhokanson1@ecc-jaguar.csuchico.edu>
2017-10-18 16:50:10 -0700

    Star Matrix

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int rows;
+  int columns;
+  cout << "Enter rows and columns of stars\n";
+  cout << endl;
+  cin >> rows >> columns;
+  vector< vector <char> > matrix(rows, vector <char> (columns));
+  for(int i = 0; i < matrix.size(); i++)
+  {
+    for(int j = 0; j < matrix[i].size(); j++)
+    {
+      matrix[i][j] = '*';
+    }
+  }
+  
+  for(int i = 0; i < matrix.size(); i++)
+  {
+    cout << endl;
+    for(int j = 0; j < matrix[i].size(); j++)
+     { 
+      cout << matrix[i][j];
+     } 
+  }
+
+  return 0;    
+}
