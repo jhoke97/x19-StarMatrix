d1562f8cd4bd829f65dc4b31a9cd27e84ab96af9
jhoke97 <jhokanson1@ecc-jaguar.csuchico.edu>
2017-10-20 10:14:04 -0700

    Updated

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@ int main()
 {
   int rows;
   int columns;
-  cout << "Enter rows and columns of stars\n";
-  cout << endl;
+  cout << "Enter rows and columns of stars:\n";
   cin >> rows >> columns;
   vector< vector <char> > matrix(rows, vector <char> (columns));
   for(int i = 0; i < matrix.size(); i++)
