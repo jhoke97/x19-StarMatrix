dc63ede62245b800762453fc38efd12b79b942d5
jhoke97 <jhokanson1@ecc-jaguar.csuchico.edu>
2017-10-20 10:38:34 -0700

    update

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
   int rows;
   int columns;
-  cout << "Enter rows and columns of stars:\n";
+  cout << "Enter rows and columns of stars:";
   cin >> rows >> columns;
   vector< vector <char> > matrix(rows, vector <char> (columns));
   for(int i = 0; i < matrix.size(); i++)
@@ -20,11 +20,11 @@ int main()
   
   for(int i = 0; i < matrix.size(); i++)
   {
-    cout << endl;
     for(int j = 0; j < matrix[i].size(); j++)
      { 
       cout << matrix[i][j];
      } 
+    cout << endl;
   }
 
   return 0;    
